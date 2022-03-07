39fcf5859f0fb2f79db721fab1358d0fb47373de
Ryan Ashiruma <ryananyangu@gmail.com>
2022-03-07 17:23:45 +0300

    completed

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,43 @@
-// import Head from "next/head";
-// import Image from "next/image";
-// import { useRef } from "react";
-// import styles from "../styles/Home.module.css";
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as bodyPix from "@tensorflow-models/body-pix";
 
+const modelConfig = {
+  architecture: "MobileNetV1",
+  outputStride: 16,
+  multiplier: 1,
+  quantBytes: 4,
+};
+
 export default function Home() {
+  let ctx_out, video_in, ctx_tmp, c_tmp, c_out, model;
+
   const processedVid = useRef();
   const rawVideo = useRef();
   const startBtn = useRef();
   const closeBtn = useRef();
   const videoDownloadRef = useRef();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (model) return;
+
+    const start_time = Date.now() / 1000;
+
+    bodyPix.load(modelConfig).then((m) => {
+      model = m;
+      const end_time = Date.now() / 1000;
+      setLoading(false);
+      console.log(`model loaded successfully, ${end_time - start_time}`);
+    });
+  });
 
   const segmentationConfig = {
     internalResolution: "full",
-    segmentationThreshold: 0.01,
-    scoreThreshold: 0.01,
-  };
-
-  const modelConfig = {
-    architecture: "MobileNetV1",
-    outputStride: 16,
-    multiplier: 1,
-    quantBytes: 4,
+    segmentationThreshold: 0.1,
+    scoreThreshold: 0.4,
+    flipHorizontal: true,
+    maxDetections: 1,
   };
 
   let recordedChunks = [];
@@ -31,7 +45,6 @@ export default function Home() {
   let options = { mimeType: "video/webm; codecs=vp9" };
   let mediaRecorder = null;
   let videoUrl = null;
-  let model;
 
   const startCamHandler = async () => {
     console.log("Starting webcam and mic ..... ");
@@ -44,16 +57,12 @@ export default function Home() {
 
     //populate video element
     rawVideo.current.srcObject = localStream;
+    video_in = rawVideo.current;
     rawVideo.current.addEventListener("loadeddata", (ev) => {
       console.log("loaded data.");
-      bodyPix.load(modelConfig).then((m) => {
-        console.log("model loaded successfully");
-        model = m;
-        transform(rawVideo.current, processedVid.current);
-      });
+      transform();
     });
 
-    // recording of local video from stream
     mediaRecorder = new MediaRecorder(localStream, options);
     mediaRecorder.ondataavailable = (event) => {
       console.log("data-available");
@@ -73,15 +82,10 @@ export default function Home() {
         type: "video/webm",
       });
 
-      // FIXME:
-      // await readFile(blob).then((encoded_file) => {
-      //   uploadVideo(encoded_file);
-      // });
-
-      // process the video to remove character
-      // Send the video to cloudinary
-      // Run/ Display the video from cloudinary url
-      //FIXME: Send data to cloudinary
+      // Save original video to cloudinary
+      await readFile(blob).then((encoded_file) => {
+        uploadVideo(encoded_file);
+      });
 
       videoDownloadRef.current.href = URL.createObjectURL(blob);
       videoDownloadRef.current.download =
@@ -121,23 +125,21 @@ export default function Home() {
     }
   };
 
-  let transform = (video_in, canvas_out) => {
-    let c_tmp, context_out, ctx_tmp;
-    context_out = canvas_out.getContext("2d");
+  let transform = () => {
+    // let ;
+    c_out = processedVid.current;
+    ctx_out = c_out.getContext("2d");
 
     c_tmp = document.createElement("canvas");
     c_tmp.setAttribute("width", 800);
     c_tmp.setAttribute("height", 450);
-    c_tmp.setAttribute("clientHeight", 450);
-    c_tmp.setAttribute("clientWidth", 800);
 
     ctx_tmp = c_tmp.getContext("2d");
 
-    computeFrame(context_out, video_in, ctx_tmp, c_tmp);
+    computeFrame();
   };
 
-  let computeFrame = (context_out, video_in, ctx_tmp, c_tmp) => {
-    // let model;
+  let computeFrame = () => {
     ctx_tmp.drawImage(
       video_in,
       0,
@@ -153,36 +155,28 @@ export default function Home() {
       video_in.videoHeight
     );
 
-    model.segmentPerson(c_tmp, segmentationConfig).then((segmentation) => {
-      let output_img = context_out.getImageData(
+    model.segmentPerson(frame, segmentationConfig).then((segmentation) => {
+      let output_img = ctx_out.getImageData(
         0,
         0,
         video_in.videoWidth,
         video_in.videoHeight
       );
 
-      // // console.log("last log line experienced", output_img);
-
       for (let x = 0; x < video_in.videoWidth; x++) {
         for (let y = 0; y < video_in.videoHeight; y++) {
           let n = x + y * video_in.videoWidth;
-          // // console.log("Reached here...");
           if (segmentation.data[n] == 0) {
-            //   console.log("segmentation");
             output_img.data[n * 4] = frame.data[n * 4]; // R
             output_img.data[n * 4 + 1] = frame.data[n * 4 + 1]; // G
             output_img.data[n * 4 + 2] = frame.data[n * 4 + 2]; // B
             output_img.data[n * 4 + 3] = frame.data[n * 4 + 3]; // A
           }
-          // else {
-          // //   console.log("Didnt meet target...");
-          // // }
-          // console.log(segmentation.data[n]);
         }
       }
-      // console.log(output_img);
-      context_out.putImageData(output_img, 0, 0);
-      setTimeout(computeFrame, 0, context_out, video_in, ctx_tmp, c_tmp);
+      // console.log(segmentation);
+      ctx_out.putImageData(output_img, 0, 0);
+      setTimeout(computeFrame, 30);
     });
   };
 
@@ -192,24 +186,30 @@ export default function Home() {
         width={800}
         height={450}
         ref={rawVideo}
+        // controls
         autoPlay
         playsInline
+        // src="someone2.webm"
       ></video>
       <br />
       <canvas width={800} height={450} ref={processedVid}></canvas>
       <br />
-
-      <button onClick={startCamHandler} ref={startBtn}>
-        Start Webcam
-      </button>
-      <button onClick={stopCamHandler} ref={closeBtn}>
-        Close Webcam
-      </button>
-      <button>
-        <a ref={videoDownloadRef} href={videoUrl}>
-          Download video
-        </a>
-      </button>
+      {!loading && (
+        <>
+          <button onClick={startCamHandler} ref={startBtn}>
+            Start Webcam
+          </button>
+          <button onClick={stopCamHandler} ref={closeBtn}>
+            Close and upload original video
+          </button>
+          <button>
+            <a ref={videoDownloadRef} href={videoUrl}>
+              Get Original video
+            </a>
+          </button>
+        </>
+      )}
+      {loading && <p>Loading machine learning models...</p>}
     </div>
   );
 }
